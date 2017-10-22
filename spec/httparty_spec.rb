8841750e62b4cf00a2d733e16d6dfe239b8e9e26
AlisonAO <alison_opeloyeru@hotmail.co.uk>
2017-10-22 20:08:30 +0100

    added more tests

--- spec/httparty_spec.rb
+++ spec/httparty_spec.rb
@@ -6,32 +6,32 @@ describe 'postcode' do
     @postcode = JSON.parse(HTTParty.get('https://api.postcodes.io/postcodes/e163jt').body)
   end
 
-  it 'status should be integer' do
+  it 'Status should be integer' do
     expect(@postcode['status']).to be_kind_of(Integer)
   end
 
-  it 'postcode shoud be a present' do
+  it 'Postcode shoud be a present' do
     expect(@postcode['result']['postcode']).not_to be_empty
   end
 
-  it 'postcode shoud be a string' do
+  it 'Postcode shoud be a string' do
     expect(@postcode['result']['postcode']).to be_kind_of(String)
   end
 
-  it "postcode should include a space" do
+  it "Postcode should include a space" do
     expect(@postcode['result']['postcode']).to include(" ")
 
   end
 
-  it 'qualiy should be integer' do
+  it 'Qualiy should be integer' do
     expect(@postcode['result']['quality']).to be_kind_of(Integer)
   end
 
-  it 'quality should be a number between 1 and 9' do
+  it 'Quality should be a number between 1 and 9' do
     expect(@postcode['result']['quality']).to be_between(1, 9).inclusive
   end
 
-  it "eastings should be an integer unless its a postcode of Channel Islands or Isle of Man" do
+  it "Eastings should be an integer unless its a postcode of Channel Islands or Isle of Man" do
     if @postcode['result']['postcode'].start_with?('GY', 'IM')
       expect(@postcode['result']['eastings']).to be_nil
     else
@@ -39,7 +39,7 @@ describe 'postcode' do
     end
   end
 
-  it "northings should be an integer unless its a postcode of Channel Islands or Isle of Man" do
+  it "Northings should be an integer unless its a postcode of Channel Islands or Isle of Man" do
     if @postcode['result']['postcode'].start_with?('GY', 'IM')
       expect(@postcode['result']['northings']).to be_nil
     else
@@ -47,17 +47,76 @@ describe 'postcode' do
     end
   end
 
-  it 'country shoud be a string' do
+  it 'Country shoud be a string' do
     expect(@postcode['result']['country']).to be_kind_of(String)
   end
 
-  it 'nhs_ha shoud be a string or null' do
-    expect(@postcode['result']['nhs_ha']).to be_kind_of(String)
+  it 'NHS Health Authority shoud be a string or null' do
+    unless @postcode['result']['nhs_ha'].nil?
+      expect(@postcode['result']['nhs_ha']).to be_kind_of(String)
+    end
   end
 
-  it "admin country should be a string or nil" do
+  it "Admin country should be a string or nil" do
     unless @postcode['result']['admin_county'].nil?
       expect(@postcode['result']['admin_county']).to be_kind_of(String)
     end
   end
+
+  it "Admin distric should be a string or nil" do
+    unless @postcode['result']['admin_district'].nil?
+      expect(@postcode['result']['admin_district']).to be_kind_of(String)
+    end
+  end
+
+  it "Admin ward should be a string or nil" do
+    unless @postcode['result']['admin_ward'].nil?
+      expect(@postcode['result']['admin_ward']).to be_kind_of(String)
+    end
+  end
+
+  it 'Longitude should be a float' do
+    expect(@postcode['result']['longitude']).to be_kind_of(Float)
+  end
+
+  it "Latitude should be a float" do
+    expect(@postcode['result']['latitude']).to be_kind_of(Float)
+  end
+
+  it 'Parliamentary Constituency should be either string or nil' do
+    unless @postcode['result']['parliamentary_constituency'].nil?
+      expect(@postcode['result']['parliamentary_constituency']).to be_kind_of(String)
+    end
+  end
+
+  it "European electoral region to be a string" do
+    unless @postcode['result']['european_electoral_region'].nil?
+      expect(@postcode['result']['european_electoral_region']).to be_kind_of(String)
+    end
+  end
+
+  it "Primary Care Trust should be a string" do
+    unless @postcode['result']['primary_care_trust']
+      expect(@postcode['result']['primary_care_trust']).to be_kind_of(String)
+    end
+  end
+
+  it "Region should be a string" do
+    unless @postcode['result']['region'].nil?
+      expect(@postcode['result']['region']).to be_kind_of(String)
+    end
+  end
+
+  it "Parish should be a string" do
+    unless @postcode['result']['parish'].nil?
+      expect(@postcode['result']['parish']).to be_kind_of(String)
+    end
+  end
+
+  it "NUTS should be a string" do
+    unless @postcode['result']['codes']['nuts'].nil?
+      expect(@postcode['result']['codes']['nuts']).to be_kind_of(String)
+    end
+  end
+
 end
